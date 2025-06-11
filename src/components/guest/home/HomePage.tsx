ff1b1c9c957f094f36d73c4cde38b8bd3096b205
Ryosuke-Ha <ryosuke.0815@icloud.com>
2025-06-11 16:59:45 +0900

    fix: delete home description

--- src/components/guest/home/HomePage.tsx
+++ src/components/guest/home/HomePage.tsx
@@ -1,25 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const HomePage: React.FC = () => {
   return (
     <div className="form-container">
       <h2>ホーム</h2>
       <p>ようこそ、Nomi Matchingへ！</p>
-      <ul style={{ listStyle: "none", padding: 0 }}>
-        <li>
-          <Link to="/search">飲み友達検索</Link>
-        </li>
-        <li>
-          <Link to="/invite">オファー送信</Link>
-        </li>
-        <li>
-          <Link to="/reviews">レビュー</Link>
-        </li>
-        <li>
-          <Link to="/payment">決済</Link>
-        </li>
-      </ul>
     </div>
   );
 };
